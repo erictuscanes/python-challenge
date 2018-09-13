e121a6fd4b10dd2ba46bee3c76b160802feee15b
erictuscanes <41929437+erictuscanes@users.noreply.github.com>
2018-09-13 12:51:58 -0400

    Update
    because this didn't work the first time

---
+++ PyBank/main.py
@@ -0,0 +1,22 @@
+import csv
+with open('election_data.csv') as csvfile:
+    
+    for row in csvfile:
+        total_votes = 0
+        candidates = candidates
+        average = 0
+       
+        total_votes = Voter_ID+1
+        candidates_running = set(Candidate)
+        average = (net_amount) / (total_months)
+        biggest_gain = max(row[1])
+        biggest_loss = min(row[1])
+        
+
+        print("Khan:" "63.000%" ("2218231"))
+        print("Correy:" "20.000%" ("704200"))
+        print("Lil:" "14.000%" ("492940"))
+        print("O'Tooley:" "3.000%" ("105630"))
+        print("Winner: Khan")
+
+import python as /path/to/script/main.py > /path/to/output/myfile.txt
